7f87b0b8ab9f12184f10702e2c6265d5d61aba6e
xxx1703xxx <119849514+xxx1703xxx@users.noreply.github.com>
2024-10-18 12:37:31 +0300

    Add files via upload

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaOrderApp.Models;
+
+namespace PizzaOrderApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (user.Age >= 16)
+                {
+                    ViewBag.UserId = user.Id; 
+                    return RedirectToAction("SelectProducts");
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "You must be 16 or older to order.";
+                    return View(user);
+                }
+            }
+            return View(user);
+        }
+
+        public IActionResult SelectProducts()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SelectProducts(int productCount)
+        {
+            ViewBag.ProductCount = productCount;
+            return View("OrderForm");
+        }
+
+        public IActionResult OrderForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult OrderForm(Product[] products)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("OrderSummary", products);
+            }
+            return View();
+        }
+
+        public IActionResult OrderSummary()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PizzaOrderApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
